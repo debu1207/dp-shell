423bf47c589a86ca9b3def0528d86a05e13b0336
debu1207 <patradebu1207@gmail.com>
2024-09-11 23:11:26 +0530

    Initial Dummy error message for any command

--- app/main.c
+++ app/main.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
+#include <string.h>
+
+void evaluate(char *cmd) {
+	cmd[strlen(cmd) - 1] = '\0';
+	printf("%s: command not found\n", cmd);
+}
 
 int main() {
   printf("$ ");
@@ -7,5 +13,6 @@ int main() {
   // Wait for user input
   char input[100];
   fgets(input, 100, stdin);
+  evaluate(input);
   return 0;
 }
